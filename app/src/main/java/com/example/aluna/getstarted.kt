c02d4e98e1a665121b5b63b28cb1203df9cde173
amrinaaa <amrina0612@gmail.com>
2025-08-16 20:21:50 -0700

    add

---
+++ app/src/main/java/com/example/aluna/getstarted.kt
@@ -0,0 +1,28 @@
+package com.example.aluna
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class getstarted : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        setContentView(R.layout.activity_getstarted)
+
+        lifecycleScope.launch {
+            delay(10000)
+            startActivity(Intent(this@getstarted, regis::class.java))
+            finish()
+        }
+    }
+}
