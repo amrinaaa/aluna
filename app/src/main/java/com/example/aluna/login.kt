5beb8b1c62b9d1bb2d82b89bbc5595c31d55f737
amrinaaa <amrina0612@gmail.com>
2025-08-18 15:22:14 -0700

    update

--- app/src/main/java/com/example/aluna/login.kt
+++ app/src/main/java/com/example/aluna/login.kt
@@ -1,6 +1,8 @@
 package com.example.aluna
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,13 @@ class login : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // ambil textview dan pasang klik
+        val labelLogin = findViewById<TextView>(R.id.labelkeregis)
+        labelLogin.setOnClickListener {
+            val intent = Intent(this, regis::class.java)
+            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/example/aluna/regis.kt
+++ app/src/main/java/com/example/aluna/regis.kt
@@ -1,6 +1,8 @@
 package com.example.aluna
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,19 @@ class regis : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_regis)
+
+        // biar padding aman
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // ambil textview dan pasang klik
+        val labelLogin = findViewById<TextView>(R.id.labelkelogin)
+        labelLogin.setOnClickListener {
+            val intent = Intent(this, login::class.java)
+            startActivity(intent)
         }
     }
 }
